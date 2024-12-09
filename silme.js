961c2e7485f156bcc70a8c3e19e543539b427638
Samet Eren Terzi <s4metet@gmail.com>
2024-12-09 10:48:47 +0300

    Meyveler Yapıldı

--- silme.js
+++ silme.js
@@ -1,1 +1,21 @@
 console.log("Projeye Başladık.");
+let meyveler = [];
+let meyveAdeti = Number(prompt("Kaç tane meyve eklemek istersiniz?"));
+
+for(let i = 0; i< meyveAdeti; i++){
+    let meyveAdi = prompt("Meyve Çeşidi Giriniz:");
+    meyveler.push(meyveAdi);
+}
+console.log(meyveler);
+
+confirm("Meyve Silmek İstermisiniz")
+console.log(meyveler);
+
+let silinenMeyve = prompt("Hangi meyveyi silmek istersiniz.");
+
+for (let i = 0; i < meyveler.length; i++) {
+    if(meyveler === silinenMeyve){
+        meyveler.splice(silinenMeyve[i], 1)
+    }
+}
+console.log(meyveler);
